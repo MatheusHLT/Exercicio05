1c62bd54e9deefe2c1452e51030f9f2ef104bd31
MatheusHLT <matheus.honorato.l.teixeira@gmail.com>
2026-03-08 13:55:11 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double nota1;
+        double nota2;
+        double nota3; 
+
+        System.out.println("Digite a nota 1:");
+        nota1 = scanner.nextDouble();
+        System.out.println("Digite a nota 2:");
+        nota2 = scanner.nextDouble();
+        System.out.println("Digite a nota 3:");
+        nota3 = scanner.nextDouble();
+
+    double mediafinal = ((nota1 + nota2 + nota3) / 3);
+
+        System.out.printf("Média: %.2f", mediafinal);
+
+        scanner.close();
+    }
+}
